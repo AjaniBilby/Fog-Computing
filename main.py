1d933271fcd5149834daf5b0cc3a4d74190b47fe
Ajani Bilby <me@ajanibilby.com>
2022-05-03 16:10:00 +1000

    +Basic framework

---
+++ main.py
@@ -0,0 +1,60 @@
+from numpy import average
+from shared import *
+
+tasks = [
+	Task(cpus=4, instructions=100),
+	Task(cpus=2, instructions=100),
+	Task(cpus=2, instructions=200)
+]
+
+queue = [x for x in tasks]
+
+nodes = [
+	Node(cpus=2, ipt=10, cost=3.0),
+	Node(cpus=4, ipt=10, cost=2.0)
+]
+
+
+while True:
+	# Determine which nodes are currently not procesing a Task
+	freeNodes = []
+	for n in nodes:
+		if not n.isProcessing():
+			freeNodes.append(n)
+
+	# If all nodes are available, and there are no tasks in the queue
+	# Then all tasks have been completed
+	if len(freeNodes) == len(nodes) and len(queue) == 0:
+		break
+
+	# FIFO scheduling
+	# assign the earliest task that meets the requirement of the node
+	for (nID, n) in enumerate(freeNodes):
+		if len(queue) > 0:
+			for (qID, t) in enumerate(queue):
+				if t.meets(n):
+					n.assign(queue.pop(qID))
+					print("Assigned node {} task {} from the queue".format(nID, qID))
+
+	# Step forward the simulation
+	for n in nodes:
+		n.tick()
+
+	# Add latency to the still awaiting tasks
+	for t in queue:
+		t.wait()
+
+
+totalLatency      = sum([t.latency for t in tasks])
+totalProcessTime  = sum([t.processTime for t in tasks])
+totalCost         = sum([t.cost for t in tasks])
+
+print("Total")
+print("  Latency : {}".format(totalLatency))
+print("  Comput  : {}".format(totalProcessTime))
+print("  Cost    : {}".format(totalCost))
+
+print("Avg")
+print("  Latency : {}".format(totalLatency / len(tasks)))
+print("  Comput  : {}".format(totalProcessTime / len(tasks)))
+print("  Cost    : {}".format(totalCost / len(tasks)))
---
+++ shared.py
@@ -0,0 +1,51 @@
+class Task:
+	def __init__(self, cpus, instructions):
+		self.cpus = cpus
+		self.instrs = instructions
+		self.progress = 0
+
+		self.latency = 0
+		self.processTime = 0
+		self.cost = 0
+
+	def meets(self, node):
+		return node.cpus >= self.cpus
+
+	def wait(self):
+		self.latency = self.latency + 1
+
+	def tick(self, ipt, cost):
+		self.processTime = self.processTime + 1
+		self.progress = self.progress + ipt
+		self.cost = self.cost + cost
+
+		return self.progress > self.instrs
+
+
+class Node:
+	def __init__(self, ipt, cpus, cost):
+		self.ipt = ipt
+		self.cpus = cpus
+		self.cost = cost
+
+		self.operation = None
+
+	def costOfExecution(self, task):
+		return task.instrs/self.ipt * self.cost
+
+	def isProcessing(self):
+		return self.operation != None
+
+	def assign(self, task):
+		if self.isProcessing():
+			raise Exception("Node Already processing")
+
+		self.processing = task.instrs
+		self.operation = task
+
+	def tick(self):
+		if self.isProcessing():
+			done = self.operation.tick(self.ipt, self.cost)
+
+			if done:
+				self.operation = None
