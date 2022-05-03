7092b486930c5d91ab3408182eb8136a02d9cd2c
Ajani Bilby <me@ajanibilby.com>
2022-05-03 16:25:38 +1000

    ~refactored

--- main.py
+++ main.py
@@ -1,49 +1,60 @@
-from numpy import average
 from shared import *
 
-tasks = [
-	Task(cpus=4, instructions=100),
-	Task(cpus=2, instructions=100),
-	Task(cpus=2, instructions=200)
-]
 
-queue = [x for x in tasks]
+def Simulate(nodes, scheduler):
+	tasks = [
+		Task(cpus=4, instructions=100),
+		Task(cpus=2, instructions=100),
+		Task(cpus=2, instructions=200)
+	]
 
-nodes = [
-	Node(cpus=2, ipt=10, cost=3.0),
-	Node(cpus=4, ipt=10, cost=2.0)
-]
+	queue = [x for x in tasks]
+
+	while True:
+		# Determine which nodes are currently not procesing a Task
+		freeNodes = []
+		for n in nodes:
+			if not n.isProcessing():
+				freeNodes.append(n)
+
+		# If all nodes are available, and there are no tasks in the queue
+		# Then all tasks have been completed
+		if len(freeNodes) == len(nodes) and len(queue) == 0:
+			break
+
+		scheduler(freeNodes, queue)
+
+		# Step forward the simulation
+		for n in nodes:
+			n.tick()
+
+		# Add latency to the still awaiting tasks
+		for t in queue:
+			t.wait()
+
+	return tasks
 
 
-while True:
-	# Determine which nodes are currently not procesing a Task
-	freeNodes = []
-	for n in nodes:
-		if not n.isProcessing():
-			freeNodes.append(n)
 
-	# If all nodes are available, and there are no tasks in the queue
-	# Then all tasks have been completed
-	if len(freeNodes) == len(nodes) and len(queue) == 0:
-		break
-
+def FIFO_Schedule(freeNodes, queue):
 	# FIFO scheduling
-	# assign the earliest task that meets the requirement of the node
-	for (nID, n) in enumerate(freeNodes):
-		if len(queue) > 0:
-			for (qID, t) in enumerate(queue):
-				if t.meets(n):
-					n.assign(queue.pop(qID))
-					print("Assigned node {} task {} from the queue".format(nID, qID))
+		# assign the earliest task that meets the requirement of the node
+		for (nID, n) in enumerate(freeNodes):
+			if len(queue) > 0:
+				for (qID, t) in enumerate(queue):
+					if t.meets(n):
+						n.assign(queue.pop(qID))
+						print("Assigned node {} task {} from the queue".format(nID, qID))
 
-	# Step forward the simulation
-	for n in nodes:
-		n.tick()
 
-	# Add latency to the still awaiting tasks
-	for t in queue:
-		t.wait()
 
+tasks = Simulate(
+	nodes = [
+		Node(cpus=2, ipt=10, cost=3.0),
+		Node(cpus=4, ipt=10, cost=2.0)
+	],
+	scheduler=FIFO_Schedule
+)
 
 totalLatency      = sum([t.latency for t in tasks])
 totalProcessTime  = sum([t.processTime for t in tasks])
