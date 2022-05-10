bfddbdb35784454c9609f1ee43e860484c94f73d
Daryll Ricalde <rstevendaryll@gmail.com>
2022-05-10 12:11:35 +1000

    prepare objective functions

---
+++ PSO.py
@@ -0,0 +1,16 @@
+"""
+Particle Swarm Optimisation
+
+It is different from other optimization algorithms
+in such a way that only the objective function is needed and
+it is not dependent on the gradient or any differential form of the objective.
+
+This is a heuristic solution because we can never prove the real global
+optimal solution can be found and it is usually not.
+However, we often find that the solution found by PSO is quite close to the global optimal.
+
+https://machinelearningmastery.com/a-gentle-introduction-to-particle-swarm-optimization/
+https://analyticsindiamag.com/a-tutorial-on-particle-swarm-optimization-in-python/
+https://github.com/katyayn/Particle-Swarm-Optimization-for-Job-Shop-Scheduling/blob/master/JobShopUsingPSO.py
+https://github.com/MagallanesFito/PSO-Swarm-Intelligence
+"""
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from simulation import *
 
 
-
 def FIFO_Schedule(freeNodes, queue):
 	# FIFO scheduling
 	# assign the earliest task that meets the requirement of the node
@@ -60,17 +59,22 @@ def PrettyStatsPrint(stats):
 	print("  Cost    : {}".format(stats['avg']['cost']))
 	print("  Idle    : {}".format(stats['avg']['idle']))
 
-
-
+# Initialise TaskGenerator
 gen = TaskGenerator(
 	        cpus = [1, 8],    # [min, max]
 	instructions = [50, 500], # [min, max]
 	max          = 500        # how many tasks to be generated before simulation stops
 )
+
+#Initialise nodes
+# Node(id,ipt,cpus,memoryUsageCost,bwCost,cpuUsageCost)
 nodes = [
-	Node(0, cpus=2, ipt=150, cost=3.0),
-	Node(1, cpus=4, ipt=100, cost=2.0),
-	Node(1, cpus=8, ipt=100, cost=2.0)
+	# Node(0, cpus=2, ipt=150, cost=3.0),
+	# Node(1, cpus=4, ipt=100, cost=2.0),
+	# Node(1, cpus=8, ipt=100, cost=2.0)
+	Node(0,ipt=150,cpus=2,memoryUsageCost=3,bwCost=1,cpuUsageCost=2.5,memoryCapacity=1000),
+	Node(1,ipt=100,cpus=4,memoryUsageCost=5,bwCost=2,cpuUsageCost=3,memoryCapacity=1500),
+	Node(2,ipt=100,cpus=8,memoryUsageCost=4,bwCost=3,cpuUsageCost=4,memoryCapacity=2000)
 ]
 
 
